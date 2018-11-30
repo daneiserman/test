ed638e64f5f7c3434abeb092f4a10a7bae56f023
Даниил Эйзерман <d.eyzerman@qsoft.ru>
2018-11-30 16:33:43 +0300

    #1 / UnitTest sample

---
+++ src/MyClass.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: deyzerman
+ * Date: 09.11.18
+ * Time: 16:11
+ */
+
+class MyClass
+{
+
+    private $arr = ['foo' => 'bar'];
+
+    public function power($x, $y)
+    {
+        return pow($x, $y);
+    }
+
+    public function getArr()
+    {
+        return $this->arr;
+    }
+}
---
+++ src/MyClassTest.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: deyzerman
+ * Date: 09.11.18
+ * Time: 16:12
+ */
+require_once '../vendor/autoload.php';
+require_once 'MyClass.php';
+
+use PHPUnit\Framework\TestCase;
+
+class MyClassTest extends TestCase
+{
+    public function testPower()
+    {
+        $my = new MyClass();
+        $this->assertEquals(8, $my->power(2, 3));
+    }
+
+    public function testProducerFirst()
+    {
+        $this->assertTrue(true);
+        return 'first';
+    }
+
+    public function testProducerSecond()
+    {
+        $this->assertTrue(true);
+        return 'second';
+    }
+
+    /**
+     * @depends testProducerFirst
+     * @depends testProducerSecond
+     */
+    public function testConsumer($a, $b)
+    {
+        $this->assertSame('first', $a);
+        $this->assertSame('second', $b);
+    }
+}
+
+class ErrorSuppressionTest extends TestCase
+{
+    public function testFileWriting()
+    {
+        $writer = new FileWriter;
+
+        $this->assertFalse(@$writer->write('/is-not-writeable/file', 'stuff'));
+    }
+}
+
+class FileWriter
+{
+    public function write($file, $content)
+    {
+        $file = fopen($file, 'w');
+
+        if ($file == false) {
+            return false;
+        }
+
+        // ...
+    }
+}
+
+class OutputTest extends TestCase
+{
+    public function testExpectFooActualFoo()
+    {
+        $this->expectOutputString('foo');
+        print 'foo';
+    }
+
+    public function testExpectBarActualBaz()
+    {
+        $this->expectOutputString('bar');
+        print 'baz';
+    }
+}
